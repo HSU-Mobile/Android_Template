71c0ca272065841de1b212953212f0aaa059b9c3
JGeun <pjk8926@gmail.com>
2022-12-15 00:36:36 +0900

    Remove files for study

--- Network_Example/app/src/main/java/com/hsu/mobile/network_example/ui/BreedViewModel.kt
+++ Network_Example/app/src/main/java/com/hsu/mobile/network_example/ui/BreedViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hsu.mobile.network_example.model.Breed
-import com.hsu.mobile.network_example.network.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,15 +16,6 @@ class BreedViewModel : ViewModel() {
     val breedList: LiveData<List<Breed>> get() = _breedList
 
     init {
-        RetrofitClient.getApiService().getBreeds().enqueue(object: Callback<List<Breed>> {
-            override fun onResponse(call: Call<List<Breed>>, response: Response<List<Breed>>) {
-                if (response.isSuccessful) {
-                    _breedList.value = response.body()
-                    loading.value = false
-                }
-            }
 
-            override fun onFailure(call: Call<List<Breed>>, t: Throwable) {}
-        })
     }
 }
